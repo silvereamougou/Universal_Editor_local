4b132835d8b19d9e35407c0a481a4b2ea39e938d
Amougou Silvere <138263116+silvereamougou@users.noreply.github.com>
2025-04-23 13:10:07 +0200

    Update head.js

--- blocks/head/head.js
+++ blocks/head/head.js
@@ -1,13 +1,13 @@
 export default function decorate() {
-    // Example meta tag
-    const desc = document.createElement('meta');
-    desc.name = 'description';
-    desc.content = 'Site description';
-    document.head.append(desc);
-  
-    // Adobe Universal Editor meta tag
-    const aueMeta = document.createElement('meta');
-    aueMeta.name = 'urn:adobe:aue:config:service';
-    aueMeta.content = 'https://localhost:8000';
-    document.head.append(aueMeta);
-  }
+  // Example meta tag
+  const desc = document.createElement('meta');
+  desc.name = 'description';
+  desc.content = 'Site description';
+  document.head.append(desc);
+
+  // Adobe Universal Editor meta tag
+  const aueMeta = document.createElement('meta');
+  aueMeta.name = 'urn:adobe:aue:config:service';
+  aueMeta.content = 'https://localhost:8000';  // Or the URL for your Universal Editor service
+  document.head.append(aueMeta);
+}
